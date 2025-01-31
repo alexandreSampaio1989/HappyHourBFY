a89d0932274d8e50d4e879cba7fa77426a658bae
Alexandre Sampaio <thndrmaster@gmail.com>
2025-01-31 16:31:56 -0300

    Criando template Base com extends e include

--- HappyHourBFY/settings.py
+++ HappyHourBFY/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
     'django_bootstrap5',
     'stdimage',
     'core'
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'HappyHourBFY.urls'
